9af8722b0d15d249ed6348e6fb915ef233987a30
anikinjura <anikinjura@mail.ru>
2024-09-13 15:40:23 +0300

    добавил в утилиты исключение файлов по символам в имени файла

--- core/management/utils.py
+++ core/management/utils.py
@@ -1,12 +1,11 @@
-# core/management/utils.py
-
 import os
 import logging
 
 def get_files_from_directory(directory, 
                              file_extensions=('.py', '.html', '.css', '.js'),
                              exclude_dirs=('migrations', '__pycache__'),
-                             exclude_files=('__init__.py',)):
+                             exclude_files=('__init__.py',),
+                             exclude_keywords=('_bak',)):
     """
     Возвращает содержимое всех файлов с указанными расширениями из директории и ее поддиректорий.
     Файлы и директории, которые должны быть исключены, можно задать параметрами.
@@ -15,6 +14,7 @@ def get_files_from_directory(directory,
     :param file_extensions: Расширения файлов, которые будут выгружаться (по умолчанию: .py, .html, .css, .js)
     :param exclude_dirs: Директории, которые будут исключены из обработки (по умолчанию: migrations, __pycache__)
     :param exclude_files: Файлы, которые будут исключены (по умолчанию: __init__.py)
+    :param exclude_keywords: Слова или символы, которые должны исключить файл из выгрузки (по умолчанию: _bak)
     :return: Словарь, где ключ - это путь к файлу, а значение - его содержимое
     """
     file_contents = {}
@@ -24,8 +24,10 @@ def get_files_from_directory(directory,
         dirs[:] = [d for d in dirs if d not in exclude_dirs]
         
         for file in files:
-            # Проверяем, чтобы файл подходил по расширению и не был в списке исключенных файлов
-            if file.endswith(file_extensions) and file not in exclude_files:
+            # Проверяем, чтобы файл подходил по расширению, не был исключен и не содержал исключенных ключевых слов
+            if (file.endswith(file_extensions) and 
+                file not in exclude_files and 
+                not any(keyword in file for keyword in exclude_keywords)):
                 file_path = os.path.join(root, file)
                 try:
                     with open(file_path, 'r', encoding='utf-8') as infile:
@@ -33,9 +35,8 @@ def get_files_from_directory(directory,
                         # Сохраняем содержимое файла в словарь
                         file_contents[file_path] = code
                 except Exception as e:
-                    # В случае ошибки с чтением файла, просто пропускаем его и добавляем информацию в логи
+                    # Логируем ошибки при чтении файлов
                     logging.error(f"Ошибка при чтении файла {file_path}: {str(e)}")
                     continue
     
     return file_contents
-
